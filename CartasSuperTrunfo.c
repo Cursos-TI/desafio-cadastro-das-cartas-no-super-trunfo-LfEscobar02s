8195fe71d23d634cd2f419481f4f929c076cc322
LfEscobar02s <lfescobar173@gmail.com>
2025-02-25 20:31:51 +0000

    Atividade Pronta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,18 +5,87 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 // começo
+
+ //Honestamente eu não tinha entendido muito bem o conteúdo.
+
 int main() {
+   /* Declaração da Carta1 */
+   char estado1;
+   char codigo1[4];
+   char nome1[50];
+   int pop1;
+   float area1;
+   float pib1;
+   int pontturist1;
 
-    char Alagoas = 1;
-    char Carta = 1;
-    char NomeDaCidade = 1;
-    int Popu = 50831;
-    float Area = 308058;
-    float PIB = 154813711;
-    int PontosTuristicos = 9;
+ //Eu demorei muito tempo para entender como o "char".
+ //Porém, depois de alguns dias de pesquisa eu consegui entender o suficiente pra fazer os codigos, de uma forma que não tivesse erros.
+
+   /* Declaração para a Carta2 */
+   char estado2;
+   char codigo2[4];
+   char nome2[50];
+   int pop2;
+   float area2;
+   float pib2;
+   int pontturist2;
+
+ //Nessas pesquisas eu descobri as seguintes coisas abaixo, como o espaço antes do "%c", e o "[^n]"
+
+   /* Entrada da Carta1 */
+   printf("Insira os dados da Carta 1:\n");
+   printf("Estado (A a H): ");
+   scanf(" %c", &estado1); //o espaço antes do "%c" ignora espaços anteriores em branco 
+   printf("Codigo da Carta (ex: A01): ");
+   scanf("%s", codigo1);
+   printf("Nome da Cidade: ");
+   scanf(" %c[^\n]", nome1); //Lê a linha até encontrar '\n'
+   printf("Populacao: ");
+   scanf("%d", &pop1);
+   printf("Area (em km²): ");
+   scanf("%f", &area1);
+   printf("PIB (em bilhoes de reais): ");
+   scanf("%f", &pib1);
+   printf("Numero de Pontos Turisticos: ");
+   scanf("%d", &pontturist1);
+
+   /* Entrada da Carta2 */
+   printf("Insira os dados da Carta 1:\n");
+   printf("Estado (A a H): ");
+   scanf(" %c", &estado2);
+   printf("Codigo da Carta (ex: A01): ");
+   scanf("%s", codigo2);
+   printf("Nome da Cidade: ");
+   scanf(" %[^\n]", nome2);
+   printf("Populacao: ");
+   scanf("%d", &pop2);
+   printf("Area (em km²): ");
+   scanf("%f", &area2);
+   printf("PIB (em bilhoes de reais): ");
+   scanf("%f", &pib2);
+   printf("Numero de Pontos Turisticos: ");
+   scanf("%d", &pontturist2);
 
 
+    /* Exibição da Carta1 */
+     printf("\nCarta 1:\n");
+     printf("Estado: %c\n", estado1);
+     printf("Codigo: %s\n", codigo1);
+     printf("Nome da Cidade: %s\n", nome1);
+     printf("Populacao: %d\n", pop1);
+     printf("Area: %.2f km²\n", area1);
+     printf("PIB: %.2f bilhoes de reais\n", pib1);
+     printf("Numero de Pontos Turisticos: %d\n", pontturist1);
 
+    /* Exibição da Carta2 */
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nome2);
+    printf("Populacao: %d\n", pop2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontturist2);
 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -30,5 +99,6 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+//Eu não entendi muito bem como a sincronização funciona, pois em um dos videos aparece outro codespace, talvez eu tenha entendido errado.
     return 0;
-}
+} 
